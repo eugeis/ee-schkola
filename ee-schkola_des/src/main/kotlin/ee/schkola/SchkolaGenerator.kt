ff8692ccc84029b9603209ab6c313aa56647a6b6
Eugen Eisler <eoeisler@gmail.com>
2017-02-12 22:43:02 +0100

    Improve Generators to generate into same file

--- ee-schkola_des/src/main/kotlin/ee/schkola/SchkolaGenerator.kt
+++ ee-schkola_des/src/main/kotlin/ee/schkola/SchkolaGenerator.kt
@@ -1,9 +1,9 @@
 package ee.schkola
 
-import ee.design.KotlinGenerator
+import ee.design.gen.DesingKotlinGenerator
 import ee.lang.integ.eePath
 
 fun main(args: Array<String>) {
-    val generator = KotlinGenerator(model())
+    val generator = DesingKotlinGenerator(model())
     generator.generate(eePath)
 }
