15cc64da72110d5000026b1712320d81c6d0037d
Eugen Eisler <eoeisler@gmail.com>
2018-03-16 16:20:50 +0100

    Work on Swagger To Model converter

--- ee-schkola_des/src/main/kotlin/ee/schkola/SchkolaGenerator.kt
+++ ee-schkola_des/src/main/kotlin/ee/schkola/SchkolaGenerator.kt
@@ -1,12 +1,13 @@
 package ee.schkola
 
 import ee.design.gen.go.DesignGoGenerator
+import ee.design.gen.kt.DesignKotlinGenerator
 import ee.lang.integ.dPath
 
 fun main(args: Array<String>) {
-    //val kotlinGenerator = DesignKotlinGenerator(Schkola)
-    val goGenerator = DesignGoGenerator(Schkola)
-    goGenerator.generate(dPath)
+    //val generator = DesignKotlinGenerator(Schkola)
+    val generator = DesignGoGenerator(Schkola)
+    generator.generate(dPath)
     //val typeScriptGenerator = DesignTypeScriptGenerator(Schkola)
     //typeScriptGenerator.generate(dPath)
 }
