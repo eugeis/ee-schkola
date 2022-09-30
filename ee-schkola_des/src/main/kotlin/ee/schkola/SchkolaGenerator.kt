16a9aa21e1b8809a4b69791bf94a4629bcbece2e
z0042vtw <erwin.wijaya@siemens.com>
2022-09-30 12:30:10 +0200

    Separating Angular & TS Template

--- ee-schkola_des/src/main/kotlin/ee/schkola/SchkolaGenerator.kt
+++ ee-schkola_des/src/main/kotlin/ee/schkola/SchkolaGenerator.kt
@@ -3,12 +3,18 @@ package ee.schkola
 import ee.design.gen.angular.DesignAngularGenerator
 import ee.design.gen.go.DesignGoGenerator
 import ee.design.gen.kt.DesignKotlinGenerator
+import ee.design.gen.ts.DesignTypeScriptGenerator
 import ee.lang.integ.dPath
 
 fun main(args: Array<String>) {
     //val generator = DesignKotlinGenerator(Schkola)
-    val generator = DesignGoGenerator(Schkola)
-    generator.generate(dPath.toRealPath())
+    // val generator = DesignGoGenerator(Schkola)
+    // generator.generate(dPath.toRealPath())
+
+    val generatorTypeScript = DesignTypeScriptGenerator(Schkola)
+    val generatorAngular = DesignAngularGenerator(Schkola)
+    generatorTypeScript.generate(dPath)
+    generatorAngular.generate(dPath)
     //val typeScriptGenerator = DesignTypeScriptGenerator(Schkola)
     //typeScriptGenerator.generate(dPath)
 }
