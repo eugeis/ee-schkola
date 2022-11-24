ba54e65bb3470d744ac45f89812b84b635b4240a
Erwin Wijaya <erwin.wijaya@siemens.com>
2022-11-24 11:34:58 +0100

    Fixing Provider on Template

--- ee-schkola_ng/src/app/shared/ee/template/template.module.ts
+++ ee-schkola_ng/src/app/shared/ee/template/template.module.ts
@@ -7,25 +7,10 @@ import {PageComponent} from './components/page/page.component';
 import {TableComponent} from './components/table/table.component';
 
 import {TemplateRoutingModules} from './template-routing.modules';
-import {ProfileDataService} from '@schkola/person/profile/service/profile-data.service';
-import {ChurchDataService} from '@schkola/person/church/service/church-data.service'
 import {FormService} from './services/form.service';
 import {TableDataService} from './services/data.service';
 import {ButtonService} from './services/button.service';
 import {MaterialModule} from './material.module';
-import {GraduationDataService} from '@schkola/person/graduation/service/graduation-data.service';
-import {ExpenseDataService} from '@schkola/finance/expense/service/expense-data.service';
-import {ExpensePurposeDataService} from '@schkola/finance/expensepurpose/service/expensepurpose-data.service';
-import {FeeDataService} from '@schkola/finance/fee/service/fee-data.service';
-import {FeeKindDataService} from '@schkola/finance/feekind/service/feekind-data.service';
-import {SchoolApplicationDataService} from '@schkola/student/schoolapplication/service/schoolapplication-data.service';
-import {SchoolYearDataService} from '@schkola/student/schoolyear/service/schoolyear-data.service';
-import {CourseDataService} from '@schkola/student/course/service/course-data.service';
-import {GroupDataService} from '@schkola/student/group/service/group-data.service';
-import {Grade} from '@schkola/student/StudentApiBase';
-import {GradeDataService} from '@schkola/student/grade/service/grade-data.service';
-import {AttendanceDataService} from '@schkola/student/attendance/service/attendance-data.service';
-import {BookDataService} from '@schkola/library/book/service/book-data.service';
 
 @NgModule({
     declarations: [FormComponent,
@@ -39,10 +24,6 @@ import {BookDataService} from '@schkola/library/book/service/book-data.service';
         MaterialModule,
     ],
     providers: [
-        ProfileDataService, ChurchDataService, GraduationDataService,
-        ExpenseDataService, ExpensePurposeDataService, FeeDataService, FeeKindDataService,
-        SchoolApplicationDataService, SchoolYearDataService, CourseDataService, GroupDataService, GradeDataService, AttendanceDataService,
-        BookDataService,
         TableDataService, ButtonService, FormService
     ],
     exports: [
