d31b2829cd7e6629160f54a6e4fe418b0235fca4
Eugen Eisler <eugen.eisler@siemens.com>
2023-03-18 20:20:27 +0100

    Clean up

--- ee-schkola_fx/src/main/kotlin/ee/schkola/fx/app/MyApp.kt
+++
@@ -1,6 +0,0 @@
-package ee.schkola.fx.app
-
-import ee.schkola.fx.view.MainView
-import tornadofx.App
-
-class MyApp : App(MainView::class, Styles::class)
--- ee-schkola_groovy/src/main/kotlin/ee/schkola/SchoolData.groovy
+++
@@ -1,8 +0,0 @@
-package ee.schkola
-
-class SchoolData {
-    List rollen, schuljahrs, ausgabenzwecks, beitragtypes,
-         dienste, dokumenttypes, gemeinden, persons, termins, fachs,
-         gruppen, anwesenheiten, ausgaben, beitrags, dokuments, noten
-    List errors
-}
